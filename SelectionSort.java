4295174158b509708f2191137c40c413df50124f
TheRealCass <ahmrc1703@learning.icmanitoba.ca>
2020-06-07 17:28:06 -0500

    find min works

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * SelectionSort
  */
@@ -6,11 +8,19 @@ public class SelectionSort {
 
     public static void main(String[] args) {
         int[] input = { 4, 2, 9, 6, 23, 12, 34, 0, 1 }; // array to be sorted
-        SelectionSort.sort(input);
+        //selectionSortv2(input);
+
+
+
+
+
+
+        //System.out.println(findMinV101(input, 0, 3));
+
+
     }
 
-    // ======================================================END
-    // PRODUCT=====================================//
+    // =====================END PRODUCT=====================================//
 
     /**
      * non recursive selection sort
@@ -23,17 +33,20 @@ public class SelectionSort {
     }
 
     /**
-     * *******add description******
-     * 
-     * @param array
-     * @param start
-     * @param end
-     * @return ***add return statement***
+     * finds the index of the smallest item in an array within a given bound
+     * @param array array to go through
+     * @param start index to start looking from
+     * @param end index to end looking
+     * @return the index of the smallest item in the array
      */
     private static int findMin(int[] array, int start, int end) {
-
-        return 0;
-
+        int index = start;
+        for (int i = start; i < end; i++) {
+            if( array[index] > array[i]) {
+                index = i;
+            }
+        }    
+        return index;
     }
 
 
@@ -62,6 +75,42 @@ public class SelectionSort {
             
         }
     }
+
+    /**
+     * v2
+     */
+    public static void selectionSortv2(int[] arr){
+
+        System.out.println("to sort:  " + Arrays.toString(arr));
+
+        for(int i = 0; i < arr.length-1; i++){
+            int min = i;
+            for(int j = i+1; j < arr.length; j++){    //选出之后待排序中值最小的位置
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            if(min != i){
+                int temp = arr[min];      //交换操作
+                arr[min] = arr[i];
+                arr[i] = temp;
+                System.out.println("Sorting:  " + Arrays.toString(arr));
+            }
+        }
+
+        System.out.println("Sorted:  " + Arrays.toString(arr));
+
+    }
+
+    /**
+     * v1.1
+     * @param toSort
+     */
+    public static void sortV101(int[] toSort) {
+
+    }
+    
+
 
 
 
